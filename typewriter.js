80a4907ec404baddd936bca3599c034a6cd135cf
Qais Nizami <qais.nizami@gmail.com>
2023-08-09 07:51:47 -0400

    Updated typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,4 +1,12 @@
 const sentence = "hello there from lighthouse labs";
-for (const char of sentence) {
-    console.log(char);
+let index = 0;
+
+const interval = setInterval(() => {
+  if (index < sentence.length) {
+    process.stdout.write(sentence[index]);
+    index++;
+  } else {
+    clearInterval(interval); // Stop the interval when all characters are printed
+    console.log();
   }
+}, 100);
